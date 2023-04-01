19379f781407ff5a7a86ae9b6035425b9060fcc9
DM <happydm09@naver.com>
2023-04-01 20:59:09 +0900

    Update 1.js

--- Blog/post/Data/1.js
+++ Blog/post/Data/1.js
@@ -14,7 +14,7 @@ function Kor() {
   date.innerHTML = '2023.04.01'
   topic.innerHTML = 'solved.ac'
   content.innerHTML = `
-만우절을 맞아 solved ac에서 41이 들어간 문제를 풀면 배지를 받을 수 있었습니다.<br> 저는 <a href='https://www.acmicpc.net/problem/23841'>23841번</a>을 풀고 배지를 받았습니다. <br> 그리고 유저 페이지가 code forces처럼 변했더군요.<a href='https://solved.ac/profile/dongmin/c'>(보기)</a> <br><br> 간만에 신선했던 이벤트였던 것 같습니다.<br><br> <img class='img-fluid' alt="" src="https://user-images.githubusercontent.com/112751504/229281107-dd3caee8-ba5c-45d1-9788-cc39992f9da7.png"></img><br>`
+만우절을 맞아 solved ac에서 41이 들어간 문제를 풀면 <a href='https://solved.ac/badges/event230401'>배지</a>를 받을 수 있었습니다.<br> 저는 <a href='https://www.acmicpc.net/problem/23841'>23841번</a>을 풀고 배지를 받았습니다. <br> 그리고 유저 페이지가 code forces처럼 변했더군요.<a href='https://solved.ac/profile/dongmin/c'>(보기)</a> <br><br> 간만에 신선했던 이벤트였던 것 같습니다.<br><br> <img class='img-fluid' alt="" src="https://user-images.githubusercontent.com/112751504/229281107-dd3caee8-ba5c-45d1-9788-cc39992f9da7.png"></img><br>`
   la = 1
 }
 
@@ -24,7 +24,7 @@ function Eng() {
   title.innerHTML = 'April Fool 2023'
   date.innerHTML = '2023.04.01'
   topic.innerHTML = 'solved.ac'
-  content.innerHTML = `For April Fool's Day, I could get a badge if I solved a problem with problem with problem number contain 41 in solved ac.<br>I solved <a href='https://www.acmicpc.net/problem/23841'>23841</a> and received the badge. <br> And the user page has changed like a code forces.<a href='https://solved.ac/profile/dongmin/c'>(View)</a> <br><br> I think it was a fresh event for the first time in a while.<br><br> <img class='img-fluid' alt="" src="https://user-images.githubusercontent.com/112751504/229281107-dd3caee8-ba5c-45d1-9788-cc39992f9da7.png"></img><br>`
+  content.innerHTML = `For April Fool's Day, I could get a <a href='https://solved.ac/badges/event230401'>badge</a> if I solved a problem with problem with problem number contain 41 in solved ac.<br>I solved <a href='https://www.acmicpc.net/problem/23841'>23841</a> and received the badge. <br> And the user page has changed like a code forces.<a href='https://solved.ac/profile/dongmin/c'>(View)</a> <br><br> I think it was a fresh event for the first time in a while.<br><br> <img class='img-fluid' alt="" src="https://user-images.githubusercontent.com/112751504/229281107-dd3caee8-ba5c-45d1-9788-cc39992f9da7.png"></img><br>`
   la = 0
 }
 
