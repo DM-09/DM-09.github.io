5abb67df9dacba8790e3c23e9ecd7fd7fc3c309d
DM <happydm09@naver.com>
2023-04-05 19:50:07 +0900

    Update Loader.js

--- Blog/post/Loader.js
+++ Blog/post/Loader.js
@@ -10,8 +10,12 @@ function LoadPost() {
 
   var m_title = document.querySelector("#m-title")
   var m_close = document.querySelector("#m-close")
+  var m_content = document.querySelector("#m-content")
+  var content = document.querySelector("#content")
+  m_content.innerHTML = ''
   m_title.innerHTML = 'No Message 메시지 없음'
   m_close.innerHTML = 'Close 닫기'
+  content.innerHTML = '<h3>No Post 글이 존재하지 않습니다.</h3>'
 }
 
 window.onload = LoadPost;
