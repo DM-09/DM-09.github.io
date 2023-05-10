0735a5188e5cc2e66ac34e68c7662c7575e15265
DM <happydm09@naver.com>
2023-05-10 20:22:43 +0900

    Update Loader.js

--- Blog/Loader.js
+++ Blog/Loader.js
@@ -6,7 +6,7 @@ function Kor() {
   var con = document.querySelector("#con")
   lang.innerHTML = 'To English'
   ab.innerHTML = `<i class="bi bi-book"></i> 이것저것 올리는 개인 블로그입니다.`
-  con.innerHTML = `<a onClick='window.open("https://dm-09.github.io/Blog/post/#1")'>[ 2023 만우절 ] - 2023.04.01ㆍsolved.ac</a><br>`
+  con.innerHTML = `<a onClick='window.open("https://dm-09.github.io/Blog/post/#2")'>[ 도장깨기 ] - 2023.05.10ㆍsolved.ac</a><br><a onClick='window.open("https://dm-09.github.io/Blog/post/#1")'>[ 2023 만우절 ] - 2023.04.01ㆍsolved.ac</a><br>`
   la = 1
 }
 
@@ -16,7 +16,7 @@ function Eng() {
   var con = document.querySelector("#con")
   lang.innerHTML = '한국어로'
   ab.innerHTML = `<i class="bi bi-book"></i> This is a personal blog where I post various things.`
-  con.innerHTML = `<a onClick='window.open("https://dm-09.github.io/Blog/post/#1")'>[ April Fool 2023 ] - 2023.04.01ㆍsolved.ac</a><br>`
+  con.innerHTML = `<a onClick='window.open("https://dm-09.github.io/Blog/post/#2")'>[ The First Solver ] - 2023.05.10ㆍsolved.ac</a><br><a onClick='window.open("https://dm-09.github.io/Blog/post/#1")'>[ April Fool 2023 ] - 2023.04.01ㆍsolved.ac</a><br>`
   la = 0
 }
 
