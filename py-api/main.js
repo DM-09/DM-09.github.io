e94d24248f17f6499559451a04d78370ad417b1a
DM <happydm09@naver.com>
2023-07-05 22:39:06 +0900

    Update main.js

--- py-api/main.js
+++ py-api/main.js
@@ -31,13 +31,13 @@ function getAPI(t) {
 }
 
 function Kor() {
-  var names = ['des', 'start', 'sde', 'plz1', 's_btn', 'test', 'ft', 'def']
+  var names = ['des', 'start', 'sde', 'plz1', 's_btn', 'test', 'ft', 'def', 'caution', 'c_de']
   var ko = ['CORS 에러를 최소화 하는 가장 쉬운 방법',
            '# 빠른 시작',
            'URL 입력 후 복사된 URL을 사용하세요',
            'API URL을 입력하세요. URL에 ~와 $가 없어야 합니다',
            '시작', '# 테스트', `API URL을 입력하세요. URL에 ~와 $가 없어야 합니다<br>버튼 클릭 후 개발자 도구를 확인하세요`,
-           '기본']
+           '기본', '# 주의', '파이썬에서 받아오고 반환하는 거라 시간이 조금 걸릴 수 있습니다.<br>에러가 나면(예: 올바르지 않은 URL일 때) Error를 반환합니다.']
   
   for (var i=0; i < names.length; i++) {
       var de = ko[i]
