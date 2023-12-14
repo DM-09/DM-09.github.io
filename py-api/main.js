121c262fe1562733885f413ff29f3279b95f8583
dongmin <happydm09@naver.com>
2023-12-14 20:03:20 +0900

    Update main.js

--- py-api/main.js
+++ py-api/main.js
@@ -1,25 +1,71 @@
-function ChangeURL(text) {
-  var new_URL = text.replace(/\//g, '~').replace(/\?/g, '$')
-  return 'https://api-py.vercel.app/' + new_URL
+var lang = 1;
+
+function Korean() {
+  lang = 1
+  $('#kor').addClass('bold')
+  $('#eng').removeClass('bold')
+  
+  $('#more').html('')
+  $('#go').html('시작')
+  $('#xhr').html('XHR(기본)')
+  $('#tform').attr('placeholder','API URL을 입력하세요')
+  $('#gform').attr('placeholder','API URL을 입력하세요')
+  
+  
+  var txt = ['소개', '사용', '테스트', '주의사항', 'py-api는 API를 불러올 때 CORS에러가 나는 것을 방지해 주는 프록시 서비스 입니다.', `위 [URL] 부분에 API URL을 넣으시면 됩니다. <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>구버전 URL</a>도 작동 합니다</p>`, `<li>Get Method만 지원합니다</li>
+        <li>파이썬을 사용하여 시간이 조금 소요됩니다</li>
+        <li>예러가 나면(잘못된 URL 등) 'Error'를 반환합니다</li>`, '개발자 도구를 사용해 확인해 보세요']
+  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de']
+  for (var i = 0; i < txt.length; i++) {
+    $(`#${ids[i]}`).html(txt[i])
+  }
 }
 
-function CopyURL() {
-  var el = document.getElementById("url")
-  var URL = ChangeURL(el.value)
+function English() {
+  lang = 0
+  $('#kor').removeClass('bold')
+  $('#eng').addClass('bold')
   
-  el.value = URL
+  $('#more').html('')
+  $('#go').html('Start')
+  $('#xhr').html('XHR(default)')
+  $('#tform').attr('placeholder','Enter the API URL')
+  $('#gform').attr('placeholder','Enter the API URL')
+  
+  var txt = ['Introduction', 'Usage', 'Test', 'Caution', 'py-api is a proxy service that prevents CORS errors when calling an API.', `Please enter the API URL in the [URL] field above. <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>Old URL</a> also works</a>`, `<li>Only Get Method is supported</li>
+        <li>This will take some time because of Python</li>
+        <li>If an error occurs (invalid URL, etc.), 'Error' is returned</li>`, 'Check the result to use developer tools']
+  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de']
+  for (var i = 0; i < txt.length; i++) {
+    $(`#${ids[i]}`).html(txt[i])
+  }
+}
+
+function MakeURL(url) {
+  return 'https://api-py.vercel.app/?r=' + url
+}
+
+function Copy() {
+  var el = document.getElementById("gform")
+  var a = el.value
+  a = MakeURL(a)
+  
+  el.value = a
   el.select();
+  
   document.execCommand("copy");
   alert('복사됨 Copied')
 }
 
-function getAPI(t) {
-  var text = document.getElementById("test_u").value
+function GetAPI(method) {
+  var url = document.getElementById("tform").value
   const xhr = new XMLHttpRequest();
   
-  if (t == 'py') { text = ChangeURL(text) }
- 
-  xhr.open('GET', text, true);
+  if (method == 'py-api') {
+    url = MakeURL(url)
+  }
+  
+  xhr.open('GET', url, true);
   xhr.responseType = 'text';
   xhr.send();
 
@@ -30,25 +76,22 @@ function getAPI(t) {
   }
 }
 
-function Kor() {
-  var names = ['des', 'start', 'sde', 'plz1', 's_btn', 'test', 'ft', 'def', 'caution', 'c_de']
-  var ko = ['CORS 에러를 최소화 하는 가장 쉬운 방법',
-           '# 빠른 시작',
-           'URL 입력 후 복사된 URL을 사용하세요',
-           'API URL을 입력하세요. URL에 ~와 $가 없어야 합니다',
-           '시작', '# 테스트', `API URL을 입력하세요. URL에 ~와 $가 없어야 합니다<br>버튼 클릭 후 개발자 도구를 확인하세요`,
-           '기본', '# 주의', '파이썬에서 받아오고 반환하는 거라 시간이 조금 걸릴 수 있습니다.<br>에러가 나면(예: 올바르지 않은 URL일 때) Error를 반환합니다.<br> Method는 Get만 지원 합니다']
+function main() {
+  var l = navigator.language
   
-  for (var i=0; i < names.length; i++) {
-      var de = ko[i]
-      var n = `#${names[i]}`
-      $(n).html(de)
-  }
+  if (l == 'ko-KR') { Korean(); lang = 1 }
+  else { English(); lang = 0 }
+}
+
+function oldURLde() {
+  var a = `Old version URL: URL with / and ? that converted to ~ and $, respectively. <br>
+ex: https://api-py.vercel.app/https:~~example.com~api$a=1`
+  
+  if (lang) { a = '구버전 URL: /, ?가 각각 ~, $로 변환된 URL <br> ex: https://api-py.vercel.app/https:~~example.com~api$a=1' }
+  
+  $('#more').html(a)
 }
 
 window.onload = function() {
-  var l = navigator.language
-  if (l == 'ko-KR') {
-    Kor()
-  }
+  main()
 };
