c3607bf3bc2c5158a6495cd05e26f30b3b7eb278
dongmin <happydm09@naver.com>
2025-03-23 10:48:42 +0900

    Update main.js

--- py-api/main.js
+++ py-api/main.js
@@ -1,4 +1,5 @@
-var lang = 1;
+var lang = 1
+var method = 1
 
 function Korean() {
   lang = 1
@@ -12,13 +13,15 @@ function Korean() {
   $('#gform').attr('placeholder','API URL을 입력하세요')
   
   
-  var txt = ['소개', '사용', '테스트', '주의사항', 'py-api는 API를 불러올 때 CORS에러가 나는 것을 방지해 주는 프록시 서비스 입니다.', `위 [URL] 부분에 API URL을 넣으시면 됩니다. <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>구버전 URL</a>도 작동 합니다</p>`, `<li>Get Method만 지원합니다</li>
-        <li>파이썬을 사용하여 시간이 조금 소요됩니다</li>
-        <li>예러가 나면(잘못된 URL 등) 'Error'를 반환합니다</li>`, '개발자 도구를 사용해 확인해 보세요']
-  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de']
+  var txt = ['소개', '사용', '테스트', '주의사항', 'py-api는 API를 불러올 때 CORS에러가 나는 것을 방지해 주는 프록시 서비스 입니다.', `위 [URL] 부분에 API URL을 넣으시면 됩니다. <span id='mde'></span> <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>구버전 URL</a>도 작동 합니다</p>`, `<li>Get, Post Method만 지원합니다</li>
+        <li>파이썬을 사용하여 시간이 조금 소요됩니다</li><li>json 형식의 응답만 지원합니다</li>`, '개발자 도구를 사용해 확인해 보세요', '에러코드', '코드', '내용', 'body의 json이 형식이 올바르지 않음', 'POST 요청시 에러 발생(잘못된 URL 등)', 'GET 요청시 에러 발생(잘못된 URL 등)', '응답이 json이 아니거나 json 형식이 어긋남']
+  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de', 'ecode', 'col-c', 'col-n', 'c1', 'c2', 'c3', 'c4']
   for (var i = 0; i < txt.length; i++) {
     $(`#${ids[i]}`).html(txt[i])
   }
+  
+  if (method == 1) { GetMethod() }
+  if (method == 2) { PostMethod() }
 }
 
 function English() {
@@ -32,13 +35,15 @@ function English() {
   $('#tform').attr('placeholder','Enter the API URL')
   $('#gform').attr('placeholder','Enter the API URL')
   
-  var txt = ['Introduction', 'Usage', 'Test', 'Caution', 'py-api is a proxy service that prevents CORS errors when calling an API.', `Please enter the API URL in the [URL] field above. <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>Old URL</a> also works</a>`, `<li>Only Get Method is supported</li>
-        <li>This will take some time because of Python</li>
-        <li>If an error occurs (invalid URL, etc.), 'Error' is returned</li>`, 'Check the result to use developer tools']
-  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de']
+  var txt = ['Introduction', 'Usage', 'Test', 'Caution', 'py-api is a proxy service that prevents CORS errors when calling an API.', `Please enter the API URL in the [URL] field above. <span id='mde'></span> <br> <p><i class="fa-solid fa-circle-info"></i> <a href="#" onClick='oldURLde()'>Old URL</a> also works</a>`, `<li>Only Get, Post Method is supported</li>
+<li>It takes a little time because it uses Python</li><li>Only JSON format response is supported</li>`, 'Check the result to use developer tools', 'Error Code', 'Code', 'Content', "body's json is not in the correct format", 'An error occurred during a POST request (invalid URL, etc.)', 'An error occurred during a GET request (invalid URL, etc.)', 'The response is not json or is in an invalid json format']
+  var ids = ['intro', 'usage', 'test', 'caution', 'intro_de', 'usage_des', 'caution_des', 'test_de', 'ecode', 'col-c', 'col-n', 'c1', 'c2', 'c3', 'c4']
   for (var i = 0; i < txt.length; i++) {
     $(`#${ids[i]}`).html(txt[i])
   }
+  
+  if (method == 1) { GetMethod() }
+  if (method == 2) { PostMethod() }
 }
 
 function MakeURL(url) {
@@ -50,6 +55,10 @@ function Copy() {
   var a = el.value
   a = MakeURL(a)
   
+  if (method == 2) {
+    a += '&body='+document.getElementById('body1').value+'&method=POST'
+  }
+  
   el.value = a
   el.select();
   
@@ -57,12 +66,16 @@ function Copy() {
   alert('복사됨 Copied')
 }
 
-function GetAPI(method) {
+function GetAPI(m) {
   var url = document.getElementById("tform").value
   const xhr = new XMLHttpRequest();
   
-  if (method == 'py-api') {
+  if (m == 'py-api') {
     url = MakeURL(url)
+  }
+  
+  if (m == 'py-api' && method == 2) {
+    url += '&method=POST&body='+document.getElementById('body2').value
   }
   
   xhr.open('GET', url, true);
@@ -90,6 +103,37 @@ ex: https://api-py.vercel.app/https:~~example.com~api$a=1`
   if (lang) { a = '구버전 URL: /, ?가 각각 ~, $로 변환된 URL <br> ex: https://api-py.vercel.app/https:~~example.com~api$a=1' }
   
   $('#more').html(a)
+}
+
+function GetMethod() {
+  var mark = 'https://api-py.vercel.app/?r=[URL]'
+  
+  method = 1
+  $('#mark').html(mark)
+  $('#mde').html('')
+  $('#msb').html('')
+  $('#mtb').html('')
+  $('#post').removeClass('bold')
+  $('#get').addClass('bold')
+}
+
+function PostMethod() {
+  var mark = 'https://api-py.vercel.app/?r=[URL]&body=[body]&method=POST'
+  var mde = '<br>[body]에 전송할 데이터를 json 형식으로 넣으시면 됩니다.'
+  var msb = `<textarea class="form-control nav-bg nav-color" id="body1" style="height: 70px" id='msbody'></textarea>
+  <label for="body1">Body</label>`
+  var mtb = `<textarea class="form-control nav-bg nav-color" id="body2" style="height: 70px" id='mtbody'></textarea>
+  <label for="body1">Body</label>`
+  
+  if (lang == 0) { mde = '<br>You can enter the data to be transmitted in json format in [body].' }
+  
+  method = 2
+  $('#mark').html(mark)
+  $('#mde').html(mde)
+  $('#get').removeClass('bold')
+  $('#post').addClass('bold')
+  $('#msb').html(msb)
+  $('#mtb').html(mtb)
 }
 
 window.onload = function() {
